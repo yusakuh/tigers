e4eedeec9c8269dc39951dbfae73b8c8ba1104e1
tsubaki_red1016 <tsubaki.red1016@gmail.com>
2020-04-29 10:59:16 +0900

    Spring + Gradle でアプリケーション起動

---
+++ pkg-tigers/src/main/java/com/horii/pkgtigers/PkgTigersApplication.java
@@ -0,0 +1,13 @@
+package com.horii.pkgtigers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkgTigersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkgTigersApplication.class, args);
+	}
+
+}
---
+++ pkg-tigers/src/test/java/com/horii/pkgtigers/PkgTigersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horii.pkgtigers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkgTigersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
